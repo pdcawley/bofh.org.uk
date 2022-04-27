ccf669122d06c0296b83d29aed35a80f93329fd2
Piers Cawley <pdcawley@bofh.org.uk>
2022-04-27 16:06:14 +0100

    Host some config into .dir-locals.el

---
+++ org-content/.dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((org-babel-default-inline-header-args . ((:session . "none")
+						  (:results . "replace")
+						  (:exports . "code")
+						  (:hlines . "yes")))))
+ (org-mode . ((mode . visual-line)
+	      (mode . visual-fill-column))))
