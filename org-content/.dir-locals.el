564108ee08a1d4fec1977f22179d0ab545fcbed9
Piers Cawley <pdcawley@bofh.org.uk>
2022-11-08 23:22:03 +0000

    Remove deprecated stuff from .dir-locals.el

--- org-content/.dir-locals.el
+++ org-content/.dir-locals.el
@@ -1,7 +1,4 @@
 ((nil . ((org-babel-default-inline-header-args . ((:session . "none")
-						  (:results . "replace")
-						  (:exports . "code")
-						  (:hlines . "yes")))))
- (org-mode . ((mode . visual-line)
-	      (mode . visual-fill-column))))
-
+                          (:results . "replace")
+                          (:exports . "code")
+                          (:hlines . "yes"))))))
