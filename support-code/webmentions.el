8c332640bfa50f24136e87e636e042efef9112df
Piers Cawley <piers@singingtogether.co.uk>
2025-09-06 20:36:28 +0100

    Make `wm-fetch-mentions' interactive

--- support-code/webmentions.el
+++ support-code/webmentions.el
@@ -26,55 +26,62 @@
 
 (defun wm-fetch-mentions ()
   "Fetch the webmentions of `wm-domain'."
+  (interactive)
   (use-package request
     :autoload request)
   (require 'seq)
   (require 'ht)
-  (let ((page-index 0)
-        (page-size 100)
-        (more? t)
-        (all-entries (vector))
-        entries)
-    (while more?
-      (request
-        wm-webmention-endpoint
-        :params `(("domain" . ,(or (getenv "WM_API_DOMAIN")
-                                   (error "WM_API_DOMAIN not set!")))
-                  ("token" . ,(or (getenv "WM_API_TOKEN")
-                                  (error "WM_API_TOKEN not set!")))
-                  ("page" . ,page-index)
-                  ("per-page" . ,page-size)
-                  ("sort-dir" . "up"))
-        :parser 'json-parse-buffer
-        :sync t
-        :success (cl-function
-                  (lambda (&key data &allow-other-keys)
-                    (if-let* ((entries (ht-get data "children")))
-                        (progn
-                          (setq all-entries (vconcat all-entries entries)
-                                more? (and entries (eql page-size (length entries)))
-                                page-index (1+ page-index)))
-                      (setq more? nil))))))
-    (let ((last-entry (seq-elt all-entries (1- (length all-entries)))))
-      (modify-dir-local-variable nil
-                                 'wm-last-mention-timestamp
-                                 (ht-get last-entry "wm-received")
-                                 'add-or-replace
-                                 (expand-file-name ".dir-locals.el" (pdc/site-dir "bofh"))))
-    (let ((mentions-by-filename
-           (seq-group-by
-            (-compose (-rpartial #'expand-file-name wm-data-dir)
-                      (-partial #'format "%s.json")
-                      (-partial #'secure-hash 'sha256)
-                      #'url-filename
-                      #'url-generic-parse-url
-                      (-rpartial #'ht-get "wm-target"))
-            all-entries)))
-      (make-directory wm-data-dir)
-      (pcase-dolist (`(,file . ,value-list) mentions-by-filename)
-        (with-temp-file file
-          (erase-buffer)
-          (json-insert (vconcat value-list)))))))
+  (save-current-buffer
+    (let ((page-index 0)
+          (page-size 100)
+          (more? t)
+          (all-entries (vector))
+          entries)
+      (while more?
+        (request
+          wm-webmention-endpoint
+          :params `(("domain" . ,(or (getenv "WM_API_DOMAIN")
+                                     (error "WM_API_DOMAIN not set!")))
+                    ("token" . ,(or (getenv "WM_API_TOKEN")
+                                    (error "WM_API_TOKEN not set!")))
+                    ("page" . ,page-index)
+                    ("per-page" . ,page-size)
+                    ("sort-dir" . "up"))
+          :parser 'json-parse-buffer
+          :sync t
+          :success (cl-function
+                    (lambda (&key data &allow-other-keys)
+                      (if-let* ((entries (ht-get data "children")))
+                          (progn
+                            (setq all-entries (vconcat all-entries entries)
+                                  more? (and entries (eql page-size (length entries)))
+                                  page-index (1+ page-index)))
+                        (setq more? nil))))))
+      (let ((last-entry (seq-elt all-entries (1- (length all-entries))))
+            (site-dirlocals-file (expand-file-name ".dir-locals.el" (pdc/site-dir "bofh"))))
+        (modify-dir-local-variable nil
+                                   'wm-last-mention-timestamp
+                                   (ht-get last-entry "wm-received")
+                                   'add-or-replace
+                                   site-dirlocals-file)
+        (when-let* ((buf (get-file-buffer site-dirlocals-file)))
+          (save-buffer buf)
+          (kill-buffer buf)))
+      (let ((mentions-by-filename
+             (seq-group-by
+              (-compose (-rpartial #'expand-file-name wm-data-dir)
+                        (-partial #'format "%s.json")
+                        (-partial #'secure-hash 'sha256)
+                        #'url-filename
+                        #'url-generic-parse-url
+                        (-rpartial #'ht-get "wm-target"))
+              all-entries)))
+        (unless (file-exists-p wm-data-dir)
+          (make-directory wm-data-dir))
+        (pcase-dolist (`(,file . ,value-list) mentions-by-filename)
+          (with-temp-file file
+            (erase-buffer)
+            (json-insert (vconcat value-list))))))))
 
 
 
